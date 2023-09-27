9f27d12441d73b8bb6dca88e2e487731e72ec56b
Mateusz Walczyk <mattiusw@gmail.com>
2023-09-27 10:05:02 +0200

    Increase loop to skip compare same element

--- Python/TotalNumber.py
+++ Python/TotalNumber.py
@@ -1,20 +1,18 @@
 def main():
-    print("Find two number where total amount is 9")
+    print("Find two number where total amount is 10")
     tab = [3,4,5,1,0,5]
     print(tab)
-    findNumber = 9
+    findNumber = 10
     amount = totalCount(tab, findNumber)
     print(amount)
     
 def totalCount(table, key):
     total = []
-    for i in table:
-        for j in table:
-            if i + j == key:
-                total.append(i)
-                total.append(j)
-                return total
-    return
+    for i in range(len(table)):
+        for j in range(i + 1, len(table)):  #Increase loop to skip compare same element
+            if table[i] + table[j] == key:
+                total.append([table[i], table[j]])
+    return total
 
 if __name__ == "__main__":
     main()
