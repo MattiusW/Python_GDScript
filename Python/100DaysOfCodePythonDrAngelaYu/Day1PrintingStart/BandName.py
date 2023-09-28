7dd593de3716688c96ec1631ae47b98155795cfa
Mateusz Walczyk <mattiusw@gmail.com>
2023-09-28 12:39:08 +0200

    Project to make

---
+++ Python/100DaysOfCodePythonDrAngelaYu/Day1PrintingStart/BandName.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello User I will help you make a band name!")
+    city = input("Which city did you grow up?\n")
+    pet = input("How name is you'r pet?\n")
+    print("You'r band name: " + pet + " " + city)
+
+if __name__ == "__main__":
+    main()
