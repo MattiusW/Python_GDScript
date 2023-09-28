9f724b050cd89a9b4dbc9af1a03fc9b1e466784a
Mateusz Walczyk <mattiusw@gmail.com>
2023-09-28 10:36:56 +0200

    Input function

--- Python/100DaysOfCodePythonDrAngelaYu/Day1PrintingStart/main.py
+++ Python/100DaysOfCodePythonDrAngelaYu/Day1PrintingStart/main.py
@@ -1,2 +1,6 @@
 print("Hello world!\nHello world!")
 print("Hello" + " " + "Matthew")
+
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+print("Hello " + input("What is your name? "))
