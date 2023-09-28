3bdedb679040f6dd6eab1feb61a2f02d67951d78
Mateusz Walczyk <mattiusw@gmail.com>
2023-09-28 10:43:07 +0200

    Print len of input

--- Python/100DaysOfCodePythonDrAngelaYu/Day1PrintingStart/main.py
+++ Python/100DaysOfCodePythonDrAngelaYu/Day1PrintingStart/main.py
@@ -4,3 +4,6 @@ print("Hello" + " " + "Matthew")
 # input() will get user input in console
 # Then print() will print the word "Hello" and the user input
 print("Hello " + input("What is your name? "))
+
+# Input and return size of name
+print(len(input("What is your name? ")))
