6c9a19612f5dadba35a2d7433a2c41b31e1be517
Mateusz Walczyk <mattiusw@gmail.com>
2023-09-29 11:41:41 +0200

    Type and cast

--- Python/100DaysOfCodePythonDrAngelaYu/Day2PrimitiveDateTypes/Lesson2.py
+++ Python/100DaysOfCodePythonDrAngelaYu/Day2PrimitiveDateTypes/Lesson2.py
@@ -17,3 +17,17 @@ print(3.14159)
 #Boolean
 print(True)
 print(False)
+
+#Type date
+example = 123
+print(type(example))
+castExample = str(123)
+print(type(castExample))
+
+#Cast type
+num_char = len(input("What is your name? " ))
+cast_to_string = str(num_char)
+print("You'r name has " + cast_to_string + " charactes.")
+
+print(70 + float("100.5"))
+print(str(70) + str(100))
