37ea7ab3e17b7ee3035c4fd1a83888865f32bd40
Mateusz Walczyk <mattiusw@gmail.com>
2023-09-29 10:03:11 +0200

    Let's start lesson 2

---
+++ Python/100DaysOfCodePythonDrAngelaYu/Day2PrimitiveDateTypes/Lesson2.py
@@ -0,0 +1,19 @@
+#Date Types
+
+#Sting
+print("Hello"[0])
+print("Hello"[-1])
+
+#Integer
+print(123 + 345)
+bigNumber = 123,456,789
+print(bigNumber)
+bigNumber2 = 987_654_321
+print(bigNumber2)
+
+#Float
+print(3.14159)
+
+#Boolean
+print(True)
+print(False)
