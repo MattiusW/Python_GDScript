e311dce6d9cee6c5a5f82d85e8cb3ac441a0146c
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-03 08:04:10 +0200

    Even number solution

---
+++ Python/100DaysOfCodePythonDrAngelaYu/Day3ControlFlowLogicalOperators/Task3.py
@@ -0,0 +1,8 @@
+# Which number do you want to check?
+number = int(input())
+
+# If the number can be divided by 2 with 0 remainder.
+if number % 2 == 0:
+    print("This number is even")
+else:
+    print("This number is odd")
