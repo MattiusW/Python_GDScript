5a3ffe2a08e49e085d7b143b8eac15cef6bc3a6a
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-04 11:32:20 +0200

    Game project

---
+++ Python/100DaysOfCodePythonDrAngelaYu/Day3ControlFlowLogicalOperators/TreasureIsland.py
@@ -0,0 +1,23 @@
+def main():
+    print("Welcome to Treasure Island. Your mission is to fing the treasure.")
+    leftOrRight = input('Take a choice "left" or "right" ')
+
+    if (leftOrRight == "left"):
+        swimOrWait = input('Now you "swim" or "wait "? ')
+        if (swimOrWait == "wait"):
+            doorChoice = input('Wchich door? "red", "blue" or "yellow"? ')
+            if (doorChoice == "red"):
+                print("Burned by Fire. Game over")
+            elif (doorChoice == "blue"):
+                print("Eaten by beast. Game over")
+            elif (doorChoice == "yellow"):
+                print("You win!")
+            else:
+                print("Game Over! ")
+        else:
+            print("Attacked by trout. Game over!")
+    else:
+        print("Fall into a hole. Game over!")
+
+if __name__ == "__main__":
+    main()
