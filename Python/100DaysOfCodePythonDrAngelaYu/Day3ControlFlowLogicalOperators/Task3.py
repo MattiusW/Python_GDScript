9de7910d3e3d6e5a89866339dc863b30ffdf1f91
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-04 09:52:13 +0200

    Love calculator

--- Python/100DaysOfCodePythonDrAngelaYu/Day3ControlFlowLogicalOperators/Task3.py
+++ Python/100DaysOfCodePythonDrAngelaYu/Day3ControlFlowLogicalOperators/Task3.py
@@ -26,3 +26,33 @@ elif bmi < 35:
   print(f"Your BMI is {bmi}, you are obese.")
 else:
   print(f"Your BMI is {bmi}, you are clinically obese.")
+
+##############################################
+print("The Love Calculator is calculating your score...")
+name1 = input() # What is your name?
+name2 = input() # What is their name?
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+combine_name = name1 + name2
+lover_name = combine_name.lower()
+
+t = lover_name.count("t")
+r = lover_name.count("r")
+u = lover_name.count("u")
+e = lover_name.count("e")
+first_digit = t + r + u + e
+
+l = lover_name.count("l")
+o = lover_name.count("o")
+v = lover_name.count("v")
+e = lover_name.count("e")
+second_digit = l + o + v + e
+
+score = int(str(first_digit) + str(second_digit))
+
+if (score < 10) or (score > 90):
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif (score >= 40) and (score <= 50):
+    print(f"Your score is {score}, you are alright together.")
+else:
+    print(f"Your score is {score}.")
