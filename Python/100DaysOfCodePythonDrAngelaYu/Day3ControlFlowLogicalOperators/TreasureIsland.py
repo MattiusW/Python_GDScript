4566ada0facbb8e49318b1c5c9d8efbdfac76c91
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-04 11:40:22 +0200

    Add lower() function

--- Python/100DaysOfCodePythonDrAngelaYu/Day3ControlFlowLogicalOperators/TreasureIsland.py
+++ Python/100DaysOfCodePythonDrAngelaYu/Day3ControlFlowLogicalOperators/TreasureIsland.py
@@ -1,11 +1,11 @@
 def main():
     print("Welcome to Treasure Island. Your mission is to fing the treasure.")
-    leftOrRight = input('Take a choice "left" or "right" ')
+    leftOrRight = input('Take a choice "left" or "right" ').lower()
 
     if (leftOrRight == "left"):
-        swimOrWait = input('Now you "swim" or "wait "? ')
+        swimOrWait = input('Now you "swim" or "wait "? ').lower()
         if (swimOrWait == "wait"):
-            doorChoice = input('Wchich door? "red", "blue" or "yellow"? ')
+            doorChoice = input('Wchich door? "red", "blue" or "yellow"? ').lower()
             if (doorChoice == "red"):
                 print("Burned by Fire. Game over")
             elif (doorChoice == "blue"):
