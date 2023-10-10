882f58bf6d386ed2bc9dd4c62e446c323ab341a4
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-10 07:23:42 +0200

    Print tab

--- Python/InsertionSort.py
+++ Python/InsertionSort.py
@@ -1,7 +1,8 @@
 def main():
     tab = [2,5,1,3]
-    insertSortv1(tab)
-    
+    print(insertSortv1(tab))
+    print(insertSortv2(tab))    
+
 def insertSortv1(A):
     for j in range(1, len(A)):
         key = A[j]
@@ -10,7 +11,19 @@ def insertSortv1(A):
             A[i + 1] = A[i]
             i = i - 1
         A[i + 1] = key
-    print(A)
+    return A
+
+def insertSortv2(tab):
+    i = 1
+    while (i < len(tab)):
+        key = tab[i]
+        j = i - 1
+        while j >= 0 and key < tab[j]:
+            tab[j + 1] = tab[j]
+            j -= 1
+        tab[j + 1] = key
+        i += 1
+    return tab
 
 if __name__ == "__main__":
     main()
