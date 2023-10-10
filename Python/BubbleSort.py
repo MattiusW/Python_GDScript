a59afda223705d7e4e23b777353a437bebcedebd
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-10 06:42:40 +0200

    Bubble sort simply version

--- Python/BubbleSort.py
+++ Python/BubbleSort.py
@@ -2,7 +2,8 @@ def main():
     table = [4, 1, 5, 2, 3, 6, 7]
     print("Table:", table)
     sorted = bubbleSort(table)
-    print("Sorted table:", sorted)    
+    print("Sorted table:", sorted)
+    print(simplyBubbleSort(table))    
 
 def bubbleSort(tab):
     i = 0 
@@ -14,6 +15,13 @@ def bubbleSort(tab):
                 tab[j], tab[j + 1] = tab[j + 1], tab[j] #Must swap in this way
             j += 1
         i += 1
+    return tab
+
+def simplyBubbleSort(tab):
+    for i in range(1, len(tab)):
+        for j in range(len(tab)):
+            if tab[j] > tab[i]:
+                tab[j], tab[i] = tab[i], tab[j] 
     return tab
 
 if __name__ == "__main__":
--- Python/InsertionSort.py
+++ Python/InsertionSort.py
@@ -2,7 +2,6 @@ def main():
     tab = [2,5,1,3]
     insertSortv1(tab)
     
-
 def insertSortv1(A):
     for j in range(1, len(A)):
         key = A[j]
