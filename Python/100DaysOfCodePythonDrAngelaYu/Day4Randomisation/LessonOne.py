0e2d34c8d929b011219664f545af6d2d455c5943
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-11 06:51:01 +0200

    Python list

--- Python/100DaysOfCodePythonDrAngelaYu/Day4Randomisation/LessonOne.py
+++ Python/100DaysOfCodePythonDrAngelaYu/Day4Randomisation/LessonOne.py
@@ -1,10 +1,20 @@
-import random
-import my_module
+# import random
+# import my_module
 
-random_integer = random.randint(1,10)
-print(random_integer)
+# random_integer = random.randint(1,10)
+# print(random_integer)
 
-randomFloat = random.random() * 5 # 0 between
-print(randomFloat)
+# randomFloat = random.random() * 5 # 0 between
+# print(randomFloat)
 
-print(my_module.pi)
+# print(my_module.pi)
+
+states_of_america = ["Delawere", "Pennsylvania", "New Jersey"]
+
+print(states_of_america[0])
+states_of_america[1] = "Pencilvania"
+# Add to list
+states_of_america.append("Angeland") 
+ # Add few item to list ["itemOne". "itemTwo"] etc
+states_of_america.extend(["Colorado", "North Dakota", "Montana"])
+print(states_of_america)
