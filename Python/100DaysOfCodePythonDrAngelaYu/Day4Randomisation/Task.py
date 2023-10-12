e637bdf43428f18b32394eb9975b7460e43643c5
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-12 15:23:24 +0200

    who pay task

--- Python/100DaysOfCodePythonDrAngelaYu/Day4Randomisation/Task.py
+++ Python/100DaysOfCodePythonDrAngelaYu/Day4Randomisation/Task.py
@@ -14,4 +14,14 @@ if money == 0:
 else:
     print("Heads")
 
+'''You are going to write a program that will select a random name from a list of names. 
+The person selected will have to pay for everybody's food bill.'''
 
+names = names_string.split(", ")
+# The code above converts the input into an array seperating
+#each name in the input by a comma and space.
+# 🚨 Don't change the code above 👆
+import random
+rand = random.randint(0, len(names) - 1)
+whoPay = names[rand]
+print(f"{whoPay} is going to buy the meal today!")
