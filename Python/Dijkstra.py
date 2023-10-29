34ceb1df42bc26f177ca3ab7239e91ba303018d9
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-29 07:42:12 +0100

    Move class node

--- Python/Dijkstra.py
+++ Python/Dijkstra.py
@@ -1,6 +1,25 @@
+import Node
+import queue
+from typing import List
+
+class Node:
+
+    def __init__(self):
+        self.neighbours = []
+        self.cost = 0
+        self.previous = None
+
 def main():
     print("Dijkstra's Algorithm")
 
+    n1 = Node()
+    n2 = Node()
+    n3 = Node()
+    n4 = Node()
+    n5 = Node()
+    n6 = Node()
+
+    n1.neighbours.append((2, n2))
 
 
 if __name__ == "__main__":
--- Python/Node.py
+++
@@ -1,6 +0,0 @@
-class Node:
-
-    def __init__(self):
-        self.neightbours = []
-        self.cost = 0
-        self.previous = None
