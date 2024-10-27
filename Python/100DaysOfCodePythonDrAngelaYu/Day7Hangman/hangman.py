dd9beebd78ef3b56dcfc19d22bddf802ec452d67
Mateusz Walczyk <mattiusw@gmail.com>
2024-10-27 18:59:07 +0100

    ready hangman

--- Python/100DaysOfCodePythonDrAngelaYu/Day7Hangman/hangman.py
+++ Python/100DaysOfCodePythonDrAngelaYu/Day7Hangman/hangman.py
@@ -12,39 +12,78 @@ def pick_word(words):
 def check_letter_in_word(word, guess_word):
     chance = 3
     word_lenght = len(word) - 1
-    found = False
-
+    new_guess_string = ''.join(guess_word)
+    print("Guess word: ", new_guess_string)
+    check_win = ''.join(word)
 
     while(chance >= 0):
+        found = False
         print("Life: ", chance)
         guess_letter_by_user = input("Pick letter: ").lower()
         for i in range(0, len(word)):
             if guess_letter_by_user == word[i]:
                 guess_word[i] = word[i]
+                new_guess_string = ''.join(guess_word)
                 print(f"Guest a letter: {guess_letter_by_user}")
                 found = True
 
         if not found:
             chance -= 1
 
-        print(guess_word)
+        elif new_guess_string == check_win:
+            print("You Win!")
+            print(new_guess_string)
+            return
 
+        draw_hangman(chance)
+        print(new_guess_string)
 
 def make_empty_table(word):
     word_lenght = len(word)
     empty_table = []
 
     for i in range(0, word_lenght):
-        empty_table.append('_')
+        empty_table.append('_ ')
 
     return empty_table
+
+def draw_hangman(life):
+    if life == 2:
+        print("""
+                 _ _
+                 o  |
+                    |
+                    |
+              """)
+    elif life == 1:
+        print("""
+                 _ _
+                 o  |
+                 |  |
+                    |
+              """)
+    elif life == 0:
+        print("""
+                 _ _
+                 o  |
+                ;|; |
+                    |
+              """)
+    elif life < 0:
+        print("""
+                 _ _
+                 o  |
+                ;|; |
+                ; ; |
+
+              YOU LOSE!
+              """)
+        return
 
 def main():
     words = ["turtle", "ant", "panda"]
     guess_word = pick_word(words)
-    print(guess_word)
     empty_word_table = make_empty_table(guess_word)
-    print(empty_word_table)
     check_letter_in_word(guess_word, empty_word_table)
 
 if __name__ == "__main__":
