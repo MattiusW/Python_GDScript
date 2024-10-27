f8f5ce3b96efb2712767970196419034f4f3bda8
Mateusz Walczyk <mattiusw@gmail.com>
2024-10-27 12:19:04 +0100

    replace letter in guess word

--- Python/100DaysOfCodePythonDrAngelaYu/Day7Hangman/hangman.py
+++ Python/100DaysOfCodePythonDrAngelaYu/Day7Hangman/hangman.py
@@ -3,25 +3,49 @@ import random
 def pick_word(words):
     draw_word = random.randint(0, len(words) - 1)
     word = words[draw_word]
-    return word
+    guess = []
 
-def check_letter_in_word(word):
+    for letter in word:
+        guess.append(letter)
+    return guess
+
+def check_letter_in_word(word, guess_word):
     chance = 3
     word_lenght = len(word) - 1
+    found = False
 
-    while(chance > 0):
-        guess_letter_by_user = input("Pick letter: ")
-        if guess_letter_by_user in word:
-             print("You guest")
-        else:
+
+    while(chance >= 0):
+        print("Life: ", chance)
+        guess_letter_by_user = input("Pick letter: ").lower()
+        for i in range(0, len(word)):
+            if guess_letter_by_user == word[i]:
+                guess_word[i] = word[i]
+                print(f"Guest a letter: {guess_letter_by_user}")
+                found = True
+
+        if not found:
             chance -= 1
-            print(chance)
+
+        print(guess_word)
+
+
+def make_empty_table(word):
+    word_lenght = len(word)
+    empty_table = []
+
+    for i in range(0, word_lenght):
+        empty_table.append('_')
+
+    return empty_table
 
 def main():
     words = ["turtle", "ant", "panda"]
     guess_word = pick_word(words)
     print(guess_word)
-    check_letter_in_word(guess_word)
+    empty_word_table = make_empty_table(guess_word)
+    print(empty_word_table)
+    check_letter_in_word(guess_word, empty_word_table)
 
 if __name__ == "__main__":
     main()
