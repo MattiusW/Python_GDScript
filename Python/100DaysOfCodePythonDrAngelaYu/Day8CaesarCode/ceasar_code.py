fa4e0ac87a0465cbaeadc29099508435aff8f36f
Mateusz Walczyk <mattiusw@gmail.com>
2024-11-01 12:00:58 +0100

    start loop since A value if is out of range

--- Python/100DaysOfCodePythonDrAngelaYu/Day8CaesarCode/ceasar_code.py
+++ Python/100DaysOfCodePythonDrAngelaYu/Day8CaesarCode/ceasar_code.py
@@ -1,14 +1,17 @@
 def encrypy(orginal_text, shift_amount):
-    print("Orginal text:", orginal_text)
     code_text = ""
     for i in orginal_text:
         ascii_value = ord(i)
-        print("ASCCI VALUE: ", ascii_value)
         ascii_shift = ascii_value + shift_amount
-        code_text += chr(ascii_shift)
-        print("ASCCI After loop: ", ascii_value)
-        print(ord(i))
-        print(f"Dodawane wartosci {ascii_value}")
+        if (shift_amount != 0):
+            if (ascii_shift > 90):
+                gap = ascii_shift - 90
+                ascii_new_shift = 64 + gap
+                code_text += chr(ascii_new_shift)
+            else:
+                code_text += chr(ascii_shift)
+        else:
+            code_text = orginal_text
 
     return code_text
 
@@ -18,7 +21,6 @@ def main():
     lower_text = text.upper()
     cast_int = int(shift)
     ascii_value = chr(cast_int)
-    print(f"Input value: {ascii_value}")
 
     print(encrypy(lower_text, cast_int))
 
