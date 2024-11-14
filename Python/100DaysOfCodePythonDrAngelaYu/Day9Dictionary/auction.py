75b947db4caed0dbf49d2fb79e4abb36e07f43f3
Mateusz Walczyk <mattiusw@gmail.com>
2024-11-14 16:45:26 +0100

    auction house

--- Python/100DaysOfCodePythonDrAngelaYu/Day9Dictionary/auction.py
+++ Python/100DaysOfCodePythonDrAngelaYu/Day9Dictionary/auction.py
@@ -1,10 +1,36 @@
 import os
 
-def main():
-    print("Hello World")
+def auction():
+    auctioners = {}
+    yes_anserw = "y"
+    again = "y"
 
-    # Clear terminal
-    # os.system('cls||clear')
+    while(again == yes_anserw):
+        try:
+            user = str(input("Get name: "))
+            bid = float(input("Get bid: $"))
+        except:
+            print("Get good value!")
+            continue
+
+        auctioners[user] = bid
+
+        print("Add another user?: ")
+        again = input("y or n?")
+
+        if again.lower() == yes_anserw:
+            os.system('cls||clear') # Clear terminal
+
+    return auctioners
+
+def main():
+    os.system('cls||clear') # Clear terminal
+    auctioners = auction()
+    print(auctioners)
+
+    high_user = max(auctioners, key=auctioners.get)
+    high_value = auctioners[high_user]
+    print(f"User with high bit: {high_user}, bit: {high_value}")
 
 if __name__ == "__main__":
     main()
